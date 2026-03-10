a063fca0c7c5a1686a77788597d637c16fd39486
Jung Hyun <kastor0323@gmail.com>
2026-03-10 10:38:22 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.50 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        
+        Stack<Integer> stack = new Stack<>();
+        for(String w : s.split(" ")){
+            if(w.equals("Z")){
+                stack.pop();
+            } else{
+                stack.push(Integer.parseInt(w));
+            }
+        }
+        for(int i : stack){
+            answer += i; 
+        }
+
+        
+        return answer;
+    }
+}
