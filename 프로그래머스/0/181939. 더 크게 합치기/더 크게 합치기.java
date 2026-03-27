414ed6f784d656c390b599c1321e9dc1a9cff9be
Jung Hyun <kastor0323@gmail.com>
2026-03-27 15:38:03 +0900

    [level 0] Title: 더 크게 합치기, Time: 12.53 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,15 +1,5 @@
 class Solution {
     public int solution(int a, int b) {
-        int answer = 0;
-        String arr1 = "";
-        String arr2 = "";
-        arr1 = Integer.toString(a) + Integer.toString(b);
-        arr2 = Integer.toString(b) + Integer.toString(a);
-        if(Integer.parseInt(arr1) < Integer.parseInt(arr2)){ 
-            answer = Integer.parseInt(arr2);
-        } else {          
-            answer = Integer.parseInt(arr1);
-        }
-        return answer;
+        return Math.max(Integer.parseInt(a + "" + b), Integer.parseInt(b + "" + a));
     }
 }
