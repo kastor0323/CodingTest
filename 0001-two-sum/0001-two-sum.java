75f55422e9ab8b2c1127451022cc0e3c50a5be07
Jung Hyun <kastor0323@gmail.com>
2026-05-14 22:06:54 +0900

    Time: 2 ms (99.37%), Space: 46.9 MB (72.88%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,20 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int a = 0;
-        int b = 0;
-        int[] answer = new int[2];
-        for(int i = 0; i < nums.length; i++){
-            for(int j = i + 1; j < nums.length; j++){
-                int sum = nums[i] + nums[j];
-                if(target == sum){
-                    a = i;
-                    b = j;
-                }
-            }
-        }
-        answer[0] = a;
-        answer[1] = b;
+        // <숫자 값, 해당 숫자의 인덱스>를 저장하는 해시맵
+        Map<Integer, Integer> map = new HashMap<>();
 
-        return answer;
+        for (int i = 0; i < nums.length; i++) {
+            // 내가 찾는 '짝꿍' 숫자 계산
+            int complement = target - nums[i];
+
+            // 1. 짝꿍이 이미 맵에 있다면? 정답 발견!
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+
+            // 2. 짝꿍이 없다면? 나중에 올 숫자를 위해 나를 맵에 저장
+            map.put(nums[i], i);
+        }
+
+        // 정답이 없는 경우 (문제 조건상 발생하지 않음)
+        return new int[] {};
     }
 }
