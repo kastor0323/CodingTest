29d7af9458f967917cb9d0fe38617c81661ef231
Jung Hyun <kastor0323@gmail.com>
2026-05-14 10:32:13 +0900

    Time: 2 ms (99.81%), Space: 52.5 MB (33.44%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,39 @@
+/* 
+그리드에서 row와 col에 +하면서 1이 있음 같은 섬으로 판단
+프로그래머스 네트워크 문제와 유사 dfs로 접근
+재귀함수를 생각해보고 answer++로 접근
+*/
+
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(grid[i][j] == '1'){
+                    count++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+
+    private void dfs(char[][] grid, int r, int c){
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        if(r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] == '0'){
+            return;
+        }
+
+        grid[r][c] = '0';
+
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c -1);
+        dfs(grid, r, c + 1);
+    }
+}
