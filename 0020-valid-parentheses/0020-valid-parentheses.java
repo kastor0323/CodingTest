64a4985411d3e6b1cfc12e26bd2665d4742f74d2
Jung Hyun <kastor0323@gmail.com>
2026-05-14 21:34:22 +0900

    Time: 3 ms (87.74%), Space: 43 MB (84.65%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,24 +1,25 @@
-import java.util.*;
+import java.util.Stack;
 
 class Solution {
     public boolean isValid(String s) {
-        Deque<Character> stack = new ArrayDeque<>();
+        Stack<Character> stack = new Stack<>();
 
         for (char c : s.toCharArray()) {
             if (c == '(' || c == '{' || c == '[') {
                 stack.push(c);
             } 
             else {
+               
                 if (stack.isEmpty()) return false;
                 
-                char top = stack.pop(); 
-                
+                char top = stack.pop();
+
                 if (c == ')' && top != '(') return false;
                 if (c == '}' && top != '{') return false;
                 if (c == ']' && top != '[') return false;
             }
         }
-
+        
         return stack.isEmpty();
     }
 }
