1fa1cc82c658f035f514f0dc50dd3ec66e0c5486
Jung Hyun <kastor0323@gmail.com>
2026-05-20 01:21:51 +0900

    Time: 1 ms (86.89%), Space: 45.2 MB (94.81%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,35 +1,22 @@
-/*
-visited[] boolean을 만들어서 TF방식
-
-순회하면서 Set으로 만들어서 Queue방식으로 빠질때 set을 제거하는 방식? -> 근데 이러면 다른것 순회할때 어려울 수 있을 듯
- */
 class Solution {
-    List<List<Integer>> answer;
-
     public List<List<Integer>> permute(int[] nums) {
-        int m = nums.length;
-        answer = new ArrayList<>();
-        dfs(nums, m, new ArrayList<>(), new boolean[nums.length]);
+        List<List<Integer>> answer = new ArrayList<>();
+        backtrack(new ArrayList<>(), nums, answer);
         return answer;
+    }
 
+    private void backtrack(List<Integer> curr, int[] nums, List<List<Integer>> answer){
+        if(curr.size() == nums.length){
+            answer.add(new ArrayList<>(curr));
+            return;
         }
 
-        void dfs(int[] nums, int m, List<Integer>curr, boolean[] visited){
-           
-            if(curr.size() == nums.length){
-                answer.add(new ArrayList<>(curr));
-                return;
+        for(int num : nums){
+            if(!curr.contains(num)){
+                curr.add(num);
+                backtrack(curr, nums, answer);
+                curr.remove(curr.size() - 1);
             }
-            for(int i = 0; i < nums.length; i++){
-                if(!visited[i]){
-                    curr.add(nums[i]);
-                    visited[i] = true;
-                    dfs(nums, m, curr, visited);
-                    visited[i] = false;
-                    curr.remove(curr.size() - 1);
-
-                }
         }
-        return;
     }
 }
