bce6f7b54ca03c01f65cf64bd9a64f8021e5c481
Jung Hyun <kastor0323@gmail.com>
2026-05-20 09:42:37 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 47.27 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -1,75 +1,72 @@
+/*
+기존 grid를 다루는 것처럼 생각하며 bfs 방식으로 접근해야한다 생각한다
+*/
 import java.util.*;
 
 class Solution {
-    static int n, m;
-    static int[] oil;
+    int n, m;
+    int[] oil;
     public int solution(int[][] land) {
-        int answer = 0;
-
         n = land.length;
         m = land[0].length;
         oil = new int[m];
-
+        
         boolean[][] visited = new boolean[n][m];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (land[i][j] == 1 && visited[i][j] == false) {
-                    bfs(land, visited, i, j);
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                //만약land == 1이고 방문을 안하면 dfs를 시작
+                if(land[i][j] == 1 && !visited[i][j]){
+                    dfs(land, visited, i, j);
                 }
             }
         }
-
-        answer = Arrays.stream(oil).max().getAsInt();
-        System.out.println(answer);
+        int answer = Arrays.stream(oil).max().getAsInt();
         return answer;
     }
-
-    public void bfs(int[][] land, boolean[][] visited, int x, int y) {
+    
+    void dfs(int[][] land, boolean[][] visited, int x, int y){
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{x, y});
+        q.add(new int[]{x,y});
         visited[x][y] = true;
-
-        // 상, 하, 좌, 우 이동
+        
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
-
-        // 석유 덩어리 개수
+        
         int count = 1;
-        // 석유 덩어리의 열 위치 저장
+        
         Set<Integer> set = new HashSet<>();
-
-        while (!q.isEmpty()) {
+        
+        while(!q.isEmpty()){
             int[] now = q.poll();
             set.add(now[1]);
-
-            for (int i = 0; i < 4; i++) {
+            
+            for(int i = 0; i < 4; i++){
                 int nx = now[0] + dx[i];
                 int ny = now[1] + dy[i];
-
-                // 땅 범위를 넘는 경우 생략
-                if (!checkRange(nx, ny)) {
+                
+                if(!checkRange(nx, ny)){
                     continue;
                 }
-
-                // 빈 땅이거나, 방문한 적이 있는 경우
-                if (land[nx][ny] == 1 && visited[nx][ny] == false) {
+                
+                if(land[nx][ny] == 1 && visited[nx][ny] == false){
                     q.add(new int[]{nx, ny});
                     visited[nx][ny] = true;
                     count += 1;
                 }
             }
         }
-
-        for (int index : set) {
+        
+        for(int index : set){
             oil[index] += count;
         }
     }
-
-    public boolean checkRange(int x, int y) {
-        if (x < 0 || x >= n || y < 0 || y >= m) {
+    
+    boolean checkRange(int x, int y){
+        if(x < 0 || x >= n || y < 0 || y >= m){
             return false;
         }
         return true;
     }
+    
 }
