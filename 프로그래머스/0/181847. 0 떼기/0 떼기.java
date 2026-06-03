b081c3e5c51cbed5d7cb982dedbd04be66afcd83
Jung Hyun <kastor0323@gmail.com>
2026-06-03 23:57:01 +0900

    [level 0] Title: 0 떼기, Time: 0.02 ms, Memory: 64.4 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,7 +1,6 @@
 class Solution {
     public String solution(String n_str) {
         String answer = "";
-        
         //int로 바꾸어서 0을 다 날리기
         int number = Integer.parseInt(n_str);
         answer = Integer.toString(number);
