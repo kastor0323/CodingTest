e7cc653875a86039e7627b9667ac7b9e37388064
Jung Hyun <kastor0323@gmail.com>
2026-06-23 09:35:50 +0900

    [level 0] Title: 수 조작하기 2, Time: 3.95 ms, Memory: 82.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(int[] numLog) {
+        StringBuilder answer = new StringBuilder();
+        
+        for (int i = 1; i < numLog.length; i++) {
+            int x = numLog[i] - numLog[i - 1];
+            
+            switch (x) {
+                case 1 -> answer.append("w");
+                case -1 -> answer.append("s");
+                case 10 -> answer.append("d");
+                case -10 -> answer.append("a");
+            }
+        }
+        
+        return answer.toString();
+    }
+}
